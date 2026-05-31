dbde55e916e2b3bdc5a2babb3a5ce5ec6e113e7a
saigithubcharan <battusaicharan@gmail.com>
2026-05-31 23:00:34 +0530

    Add authentication module structure

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("Task Manager API Running");
 });
---
+++ Backend/src/controllers/authController.js
@@ -0,0 +1,5 @@
+exports.register = async (req, res) => {};
+
+exports.login = async (req, res) => {};
+
+exports.getMe = async (req, res) => {};
